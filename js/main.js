2e9ebc21716f92396a99f368316098f68e4013f2
Ignacio Roca <rocanacho08@gmail.com>
2023-07-20 13:43:47 -0300

    DOM

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+function crearTienda(productos) {
+  for (let producto of productos) {
+    let productoMostrar = document.createElement("div");
+    productoMostrar.innerHTML = `<img src="./img/${producto.nombre}.webp" alt="${producto.nombre}Img">
+    <h2> ${producto.nombre} </h2>
+     <p>$${producto.precio}</p>
+     <p>stock:${producto.unidades}</p>
+     <button id=btnProducto${producto.id} class=btnStyle>Añadir al carrito</button>`;
+    document.getElementById("productos").appendChild(productoMostrar);
+
+    productoMostrar.setAttribute("id", "producto");
+  }
+}
+
+function limpiarTienda() {
+  let elemento = document.querySelectorAll("#producto");
+  console.log(elemento);
+  for (let i = 0; i < elemento.length; i++) {
+    document.getElementById("producto").remove();
+  }
+}
+
 function calcularDescuento(codigo) {
   if (codigo == "coderhouse") {
     alert("codigo de descuento aplicado.");
@@ -32,7 +54,7 @@ let Npedido = Math.round(Math.random() * 99999 + 10000);
 let nombre;
 let ubicacion;
 let productoBuscarNombre;
-let productoBuscarId = parseInt(productoBuscarNombre);
+let productoBuscarId;
 let busqueda;
 let codigoDescuento;
 
@@ -40,6 +62,32 @@ productos.push(new producto("aceite", 500, 15, 1));
 productos.push(new producto("harina", 300, 20, 2));
 productos.push(new producto("pure de tomate", 150, 7, 3));
 productos.push(new producto("sal fina", 200, 30, 4));
+
+crearTienda(productos);
+
+let productoBuscar = document.getElementById("buscar");
+productoBuscar.oninput = () => {
+  busqueda = productos.find(
+    (producto) => producto.nombre === productoBuscar.value
+  );
+  console.log(busqueda?.nombre || "producto no encontrado");
+  let valor = busqueda?.nombre || "producto no encontrado";
+  if (valor === productoBuscar.value) {
+    limpiarTienda();
+    let mostrarBusqueda = document.createElement("div");
+    mostrarBusqueda.innerHTML = `<img src="./img/${busqueda.nombre}.webp" alt="${busqueda.nombre}Img">
+   <h2> ${busqueda.nombre} </h2>
+   <p>$${busqueda.precio}</p>
+   <p>stock:${busqueda.unidades}</p>
+   <button id=btnProducto${busqueda.id} class=btnStyle>Añadir al carrito</button>`;
+    document.getElementById("productos").appendChild(mostrarBusqueda);
+
+    mostrarBusqueda.setAttribute("id", "producto");
+  } else if (valor === "producto no encontrado") {
+    limpiarTienda();
+    crearTienda(productos);
+  }
+};
 
 if (ubicacion == "buenos aires" || ubicacion == "Buenos aires") {
   envio = 0;
